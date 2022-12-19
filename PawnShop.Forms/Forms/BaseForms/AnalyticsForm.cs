21abe9bbdd8075e0c30eeed7f935f23ab4c99152
Егор Кузьмин <1203egor@gmail.com>
2022-12-19 16:28:07 +0200

    logs

--- PawnShop.Forms/Forms/BaseForms/AnalyticsForm.cs
+++ PawnShop.Forms/Forms/BaseForms/AnalyticsForm.cs
@@ -3,13 +3,7 @@ using IronPdf.Rendering.Abstractions;
 using PawnShop.Forms.Extensions;
 using PawnShop.Oracle.Services;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Configuration;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
